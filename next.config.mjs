3a57ba5451ac89f51e58efd479907113df797d4c
Rostislav <rostislav@MacBook-Pro-Rostislav.local>
2024-09-09 16:12:35 +0300

    Preporation for work, delete default files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,37 @@
+import {fileURLToPath} from 'url';
+import {dirname} from 'path';
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    webpack(config) {
+
+        // Получаем путь к текущему файлу
+        const __filename = fileURLToPath(import.meta.url);
+        const __dirname = dirname(__filename);
+
+        // Добавляем алиасы в конфигурацию webpack
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            '@/': path.resolve(__dirname, '..'),
+            '@public': path.resolve(__dirname, 'public'),
+            '@app': path.resolve(__dirname, 'app'),
+            '@components': path.resolve(__dirname, 'app/components'),
+            '@utils': path.resolve(__dirname, 'app/utils'),
+            '@types': path.resolve(__dirname, 'app/types'),
+            '@interfaces': path.resolve(__dirname, 'app/interfaces'),
+            '@styles': path.resolve(__dirname, 'app/styles'),
+        };
+
+        // Добавляем правило для обработки SVG файло
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
+
+        return config;
+    },
+};
 
 export default nextConfig;
